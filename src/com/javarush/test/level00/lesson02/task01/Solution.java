7d1815c0cc4f9c33c3a1ae6b02e45058d1e272bf
NoLiveKMS <g1478963g>
2014-10-04 16:43:44 +0400

    task01

---
+++ src/com/javarush/test/level00/lesson02/task01/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.test.level00.lesson02.task01;
+
+/**
+ * Created by noliv_000 on 04.10.2014.
+ */
+/* Быть программистом - круто!
+Напиши программу, которая выводит на экран надпись: «Я думаю, быть программистом - это круто».
+*/
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Я думаю, быть программистом - это круто");
+    }
+}
