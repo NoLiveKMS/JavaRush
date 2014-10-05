50035c6c8d6db40d4b561fee65a523ab893d3b03
NoLiveKMS <g1478963g>
2014-10-05 10:12:57 +0400

    lesson 2 complete

---
+++ src/com/javarush/test/level00/lesson03/task01/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.test.level00.lesson03.task01;
+
+/**
+ * Created by noliv_000 on 05.10.2014.
+ */
+/* Лила самая красивая
+Написать программу, выводящую на экран надпись «Лила самая красивая» 5 раз. Каждый раз – с новой строки.
+*/
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        for (int i = 0; i < 5; i++){
+            System.out.println("Лила самая красивая");
+        }
+    }
+}
---
+++ src/com/javarush/test/level00/lesson03/task03/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.test.level00.lesson03.task03;
+
+/**
+ * Created by noliv_000 on 05.10.2014.
+ */
+/* Амиго очень умный
+Написать программу, выводящую на экран надпись «Амиго очень умный».
+*/
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Амиго очень умный");
+    }
+}
